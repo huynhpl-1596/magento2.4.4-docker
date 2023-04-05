0f94a11901e2feacd0c93631ab8826a855af0548
phan.ly.huynh <phan.ly.huynh@sun-asterisk.com>
2023-04-05 15:13:51 +0700

    feat: create script and post model

---
+++ app/code/Huynhpl/HelloWorld/Model/ResourceModel/Post/Collection.php
@@ -0,0 +1,19 @@
+<?php
+namespace Huynhpl\HelloWorld\Model\ResourceModel\Post;
+
+class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
+{
+	protected $_idFieldName = 'post_id';
+	protected $_eventPrefix = 'huynhpl_helloworld_post_collection';
+	protected $_eventObject = 'post_collection';
+
+	/**
+	 * Define resource model
+	 *
+	 * @return void
+	 */
+	protected function _construct()
+	{
+		$this->_init('Huynhpl\HelloWorld\Model\Post', 'Huynhpl\HelloWorld\Model\ResourceModel\Post');
+	}
+}
